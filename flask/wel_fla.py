9382499e580dc04d642d6b82db7357eb472596b1
Fexas25 <148991740+Fexas25@users.noreply.github.com>
2024-03-22 16:55:45 +0000

    Update wel_fla.py

--- flask/wel_fla.py
+++ flask/wel_fla.py
@@ -21,6 +21,29 @@ def success():
 def datacollect():
     form = DataCollectForm()
     if form.validate_on_submit():
+        name = form.name.data
+        pnumber = form.pnumber.data
+        email = form.email.data
+        degree = form.degree.data
+        study_feeling = form.study_feeling.data
+        grade_last_exam = form.grade_last_exam.data
+        improvement = ', '.join(form.improvement.data)
+        differences = form.differences.data
+        changes = form.changes.data
+
+        # Store the form data in the data.txt file
+        with open('data.txt', 'a') as file:
+            file.write(f"Name: {name}\n")
+            file.write(f"Pnumber: {pnumber}\n")
+            file.write(f"Email: {email}\n")
+            file.write(f"Degree: {degree}\n")
+            file.write(f"Study Feeling: {study_feeling}\n")
+            file.write(f"Grade Last Exam: {grade_last_exam}\n")
+            file.write(f"Improvement: {improvement}\n")
+            file.write(f"Differences: {differences}\n")
+            file.write(f"Changes: {changes}\n")
+            file.write("\n")
+
         return redirect('/success')
     return render_template('datacollect.html', form=form)
 
